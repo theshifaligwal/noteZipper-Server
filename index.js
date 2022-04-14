d5b0570ddb2a4c43a7e45f82ca18aa776e2a3274
Shifali Gwal <gwal.shifali2223@gmail.com>
2022-04-14 17:25:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const notes = require('./DummyData/notes')
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req,res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note);
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT} port`);
+});
