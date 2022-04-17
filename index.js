3ca97863bb2913d22344b876608823bf4ba6471e
Shifali Gwal <gwal.shifali2223@gmail.com>
2022-04-17 19:54:34 +0530

    Connect to database

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const notes = require('./DummyData/notes')
 const dotenv = require("dotenv")
+const connectDB = require('./config/db')
 
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("hello world");
